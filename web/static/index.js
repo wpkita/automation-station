83b5a02e73305d71e95b26e2340afb1dfd629b40
wpkita <william.p.kita@gmail.com>
2015-02-12 20:35:54 -0800

    Experiment with adding new tasks

--- web/static/index.js
+++ web/static/index.js
@@ -1,5 +1,19 @@
 angular
   .module('automationStation', [])
   .controller('MainController', function($scope) {
-    $scope.tasks = ['HelloWorld'];
+    $scope.tasks = [{
+      name: 'HelloWorld'
+    }];
+
+    $scope.newTask = {
+      name: ''
+    };
+
+    $scope.addNewTask = function(newTask) {
+      $scope.tasks.push(newTask);
+
+      $scope.newTask = {
+        name: ''
+      };
+    }
   });
